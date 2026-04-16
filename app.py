7824c3e0da5833e384476d9ce6ceb32a7778e27c
Vivek-8885 <84636725+Vivek-8885@users.noreply.github.com>
2026-04-16 14:41:01 +0530

    added my name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/get-data', methods=['GET'])
 def get_data():
-    return jsonify({"message": "Hello from Flask API"})
+    return jsonify({"message": "Hello from Flask API Vivek Vardhan"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
